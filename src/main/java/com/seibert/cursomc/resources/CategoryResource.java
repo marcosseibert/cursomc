dced54e491b240d4c6a4037ade1d00b965c76464
Marcos Seibert <marcosantonioseibert@gmail.com>
2018-06-14 20:39:07 -0300

    Testing domain class category

--- src/main/java/com/seibert/cursomc/resources/CategoryResource.java
+++ src/main/java/com/seibert/cursomc/resources/CategoryResource.java
@@ -1,15 +1,29 @@
 package com.seibert.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.seibert.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String getList() {
-		return "Rest is working!";
+	public List<Category> getList() {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(1, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
