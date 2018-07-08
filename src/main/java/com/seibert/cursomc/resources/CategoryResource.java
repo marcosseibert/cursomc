1d6e0e2524c0c98283311f21203f75b16a6d94cd
seibert <marcosantonioseibert@gmail.com>
2018-07-08 14:35:29 -0300

    Created method insert new Category in CategoryResource

--- src/main/java/com/seibert/cursomc/resources/CategoryResource.java
+++ src/main/java/com/seibert/cursomc/resources/CategoryResource.java
@@ -1,14 +1,17 @@
 package com.seibert.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.seibert.cursomc.domain.Category;
 import com.seibert.cursomc.services.CategoryService;
@@ -25,5 +28,13 @@ public class CategoryResource {
 		
 		Category category = service.getCategory(id);
 		return ResponseEntity.ok().body(category);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").build(obj.getId());
+		return ResponseEntity.created(uri).build();
 	}
 }
