dbe1b2b1ac7ffdc80f4fcd5190ad929f77593500
marcos seibert <marcosantonioseibert@gmail.com>
2019-10-02 21:36:27 -0300

    correcting import

--- src/main/java/com/seibert/cursomc/security/JWTAuthorizationFilter.java
+++ src/main/java/com/seibert/cursomc/security/JWTAuthorizationFilter.java
@@ -1,12 +1,11 @@
 package com.seibert.cursomc.security;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
